e40058dcbfcb18cdcdf41017919dec1de330be9f
Samudra Perera <samudrapup@gmail.com>
2024-09-08 20:27:21 -0400

    About Page Updates

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import BackToMainButton from "../components/BackToMainButton";
 import Container from "../components/Container";
+import { useTheme } from "../context/ThemeContext";
 
 const AboutLinks = [
   { name: "Resume", href: "/Samudra_Resume.pdf" },
@@ -9,14 +10,22 @@ const AboutLinks = [
   { name: "Twitter", href: "https://twitter.com/theRealSamudra" },
 ];
 
+const AboutParagraphs = ({ children }) => {
+  return <p className="text-black text-xs pb-4">{children}</p>;
+};
+
 const About = () => {
+  const { accentTheme } = useTheme();
   return (
     <Container>
       <BackToMainButton />
-      <h2 className="font-bold text-4xl tracking-wide text-slate-50 pb-8 pt-4">
+      <h2
+        className={`font-bold text-2xl tracking-wide text-accent-${accentTheme} pb-8 pt-4`}
+      >
         About Me
       </h2>
-      <p className="text-slate-100 pb-4">
+      <AboutParagraphs>
+        {" "}
         Hi! Im Samudra, and I am a software engineer from Toronto, Canada. I am
         currently studying Systems Design Engineering at the{" "}
         <a
@@ -28,8 +37,8 @@ const About = () => {
         </a>{" "}
         Before I ventured into the world of tech I was a Civil Engineer for a
         few years working on major infrastruture projects.
-      </p>
-      <p className="text-slate-100 pb-4">
+      </AboutParagraphs>{" "}
+      <AboutParagraphs>
         Outside of my professional life, I'm passionate about staying active and
         embracing the joys of life with friend and family. I find balance by
         regularly hitting the gym, playing soccer with the boys or going for
@@ -38,17 +47,18 @@ const About = () => {
         <br />
         Im also an avid reader, and currently trying to reconnect with my
         creative side through different forms of digital and physical mediums.
-      </p>
-      <p className="text-slate-100 pb-4">
+      </AboutParagraphs>
+      <AboutParagraphs>
         See what I am up to{" "}
-        <Link to="/now" className="underline text-sky-300">
+        <Link
+          to="/now"
+          className={`underline decoration-accent-${accentTheme} text-black`}
+        >
           now.
         </Link>
-      </p>
-      <h3 className="font-bold text-2xl tracking-wide text-slate-50 pb-2">
-        Contact
-      </h3>
-      <ul className="text-slate-100 decoration-white list-disc pl-8">
+      </AboutParagraphs>
+      <h3 className="font-bold text-lg tracking-wide pb-2">Contact</h3>
+      <ul className="text-black text-xs list-disc pl-8">
         {AboutLinks.map((link) => {
           return (
             <li className="pb-2">
@@ -56,7 +66,7 @@ const About = () => {
                 href={link.href}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-sky-300"
+                className={`underline decoration-accent-${accentTheme} text-black`}
               >
                 {link.name}
               </a>
